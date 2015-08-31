ecab31bd51eb28188692811c74c7238047861cf9
Liz Mackie <elizabeth.mackie@acquia.com>
2015-08-31 18:15:36 -0400

    tweaked theme elements, added JS behaviors and added bg image to front page highlight section.

--- sites/all/themes/custom/twisted_rail/js/script.js
+++ sites/all/themes/custom/twisted_rail/js/script.js
@@ -17,43 +17,57 @@
 Drupal.behaviors.twistedRail = {
   attach: function(context, settings) {
 
-    // Place your code here.
+  // ------------------------------ GLOBAL VARIABLES -------------------------------
+
+  var headerHeight = $('#header').height();
+
+
+  // ------------------------------ LAYOUT -------------------------------
+  
+  // Set main top margin to same height has the header
+  $('#main').css('margin-top', headerHeight+'px');
 
 
   // ------------------------------ FADE IN PAGE -------------------------------
-
 
   $('#page').hide();
   $('#page').fadeIn(1000);
 
 
-  // ------------------------------ FADE IN/OUT LOGO -------------------------------
+  // ------------------------------ FADE IN/OUT LOGO ON FRONT PAGE -------------------------------
 
   // if the .front class exists (i.e. if on the front page)
   if ($('.front').length) {
     $('.header__logo-image').hide();
-    var elPos = $('.logo-fade').offset().top;
-    $(window).scroll(function() {
-      // Contains the position of the window
-      var scrollPos = $(this).scrollTop();
-      // if the window top is greater than the difference window and #navigation position 
-      if (scrollPos > elPos + 330) {  
-        // Fade the small logo in
-        $('.header__logo-image').fadeIn(300);
-      }
-      else {  
-        // Otherwise, fade the logo out
-        $('.header__logo-image').fadeOut(300);
-      }
+    $(window).load(function() {
+      var elHeight = $('.logo-fade').height() - headerHeight;
+      $(window).scroll(function() {
+        // Contains the position of the window
+        var scrollPos = $(this).scrollTop();
+        // if the window top is greater than the difference between #navigation position and .logo-fade height 
+        if (scrollPos > elHeight) {  
+          // Fade the small logo in
+          $('.header__logo-image').fadeIn(400);
+        }
+        else {  
+          // Otherwise, fade the logo out
+          $('.header__logo-image').fadeOut(400);
+        }
+      });
     });
+    $(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    var winHeight = $(window).height();
+
+    $('.logo-fade').css({
+        'opacity': ((winHeight - 1.5*scrollTop) / winHeight)
+    }); 
+});
+
   };
 
 
-  // Set main top margin to same height has the header
 
-  var headerHeight = $('#header').height();
-
-  $('#main').css('margin-top', headerHeight+'px');
 
 
 
