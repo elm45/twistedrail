4b3ee5c0c8b3a3830d66bb7b4530b226f0087b86
Liz Mackie <elizabeth.mackie@acquia.com>
2015-08-31 12:19:16 -0400

    Initial commit - Zen them installed and styling started.

---
+++ sites/all/themes/custom/twisted_rail/js/script.js
@@ -0,0 +1,62 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+
+// To understand behaviors, see https://drupal.org/node/756722#behaviors
+Drupal.behaviors.my_custom_behavior = {
+  attach: function(context, settings) {
+
+    // Place your code here.
+
+
+  // ------------------------------ FADE IN PAGE -------------------------------
+
+
+  $('#page').hide();
+  $('#page').fadeIn(1000);
+
+
+  // ------------------------------ FADE IN/OUT LOGO -------------------------------
+
+  // if the .front class exists (i.e. if on the front page)
+  if ($('.front').length) {
+    $('.header__logo-image').hide();
+    var elPos = $('.logo-fade').offset().top;
+    $(window).scroll(function() {
+      // Contains the position of the window
+      var scrollPos = $(this).scrollTop();
+      // if the window top is greater than the difference window and #navigation position 
+      if (scrollPos > elPos + 330) {  
+        // Fade the small logo in
+        $('.header__logo-image').fadeIn(300);
+      }
+      else {  
+        // Otherwise, fade the logo out
+        $('.header__logo-image').fadeOut(300);
+      }
+    });
+  };
+
+
+
+
+
+
+
+
+  }
+};
+
+
+})(jQuery, Drupal, this, this.document);
